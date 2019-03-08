b2beb5f2a67718f33c2a0e712a3f6dad39a97367
phantomamoeba <polonius@davistx.com>
2019-03-08 13:01:23 -0600

    initial

---
+++ photon.py
@@ -0,0 +1,3 @@
+"""
+Photon class ... handles propogation, pathing, absorption, escape, etc of a photon
+"""
---
+++ utilities.py
@@ -0,0 +1,3 @@
+"""
+Collection of utilities such as random selection of direction, conversion between coord systems, validations plotting
+"""
